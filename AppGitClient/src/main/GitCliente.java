bbfd6b8900549eeb6ec5eeb867b4fe9abc2b95e2
Juan <juanlesfuen@gmail.com>
2023-12-15 11:37:44 +0100

    Merge cambios de features/newrequest en develop

---
+++ AppGitClient/src/main/GitCliente.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author lesfueju
+ */
+public class GitCliente {
+    
+    private String cliente;
+    private String versions;
+    private String sistemaOper;
+    private String licencia;
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getVersions() {
+        return versions;
+    }
+
+    public void setVersions(String versions) {
+        this.versions = versions;
+    }
+
+    public String getSistemaOper() {
+        return sistemaOper;
+    }
+
+    public void setSistemaOper(String sistemaOper) {
+        this.sistemaOper = sistemaOper;
+    }
+
+    public String getLicencia() {
+        return licencia;
+    }
+
+    public void setLicencia(String licencia) {
+        this.licencia = licencia;
+    }
+    
+}
